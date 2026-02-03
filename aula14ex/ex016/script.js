5d192b3d14eec286fb63f702970062f607627e44
Marcos Vinícius Moreira da Silva <254775220+marcosmoreira-dev@users.noreply.github.com>
2026-02-02 22:05:49 -0300

    termina exercicio 16

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -6,14 +6,29 @@ function contar(){
     let res = document.getElementById('res')
 
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar'
+        //window.alert('[ERRO] Faltam dados!')
     } else {
         res.innerHTML = 'Contando:'
         let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            window.alert('Passo inválido! Considerando PASSO 1')
+            P = 1
+        }
+        if (i < f) {
+            // contagem crescente
+            for (let c = i; c <= f; c += p){
+                res.innerHTML += `${c}\u{1F449}`
+        }
+        } else {
+            // contagem regressiva
+            for (let c = i; c>= f; c-= p) {
+                res.innerHTML += `${c}\u{1F449}` 
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
     
-
-    for (inicio; fim < inicio; passo){
-        innerHTML 
-    }
 }
