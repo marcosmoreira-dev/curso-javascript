d537eaaff841057100c72bc459cf028e5f8caf0c
Marcos Vinícius Moreira da Silva <254775220+marcosmoreira-dev@users.noreply.github.com>
2026-02-04 16:31:08 -0300

    conclui exercicio analisador numeros

--- exercícios-propostos/exercicio-analisador-numeros/script.js
+++ exercícios-propostos/exercicio-analisador-numeros/script.js
@@ -1,21 +1,62 @@
-let numeros = []
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else {
+        return false
+    }
+} 
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
 
 function adicionar() {
-    let num = document.getElementById('txtn')
-    let n = Number(num.value)
-    if (num.value === '') {
-        return window.alert ('Você precisa digitar algum número')
-
-    } if (num.value < 1 || num.value > 100){
-        return window.alert ('Número fora do intervalo permitido (1 a 100)')
-
-    } else { 
-        if (numeros.indexOf(n) !== -1) {
-            return window.alert('Número já adicionado!')
-        }
-        numeros.push(n)
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
         let item = document.createElement('option')
-        item.text = n
-        document.getElementById('selnum').appendChild(item)
-        }
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
     }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar')
+    } else {
+        let tot = valores.length
+        let maior = valores [0]
+        let menor = valores [0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
+
+    }
+}
